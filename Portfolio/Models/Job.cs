63690fcfa8465a1ec6297fe4261cb02a03ffaad0
Andi <andicbuchanan@gmail.com>
2016-10-02 17:33:44 +1300

    added classes

---
+++ Portfolio/Models/Job.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Portfolio.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+        public string Title { get; set; }
+        public string Employer { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+    }
+}
